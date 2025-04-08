019edc852d631fda7fc0541ca5b38571dd2599e1
gara0xqm <gara.xq@gmail.com>
2025-04-08 13:53:00 +0200

    ali

--- lib/features/search/presentation/screen/home_screen.dart
+++ lib/features/search/presentation/screen/home_screen.dart
@@ -58,7 +58,7 @@ class HomeScreen extends StatelessWidget {
               children: [
                 Container(
                   width: double.infinity,
-                  height: width >= 800 ? 60 : 150,
+                  height: width >= 800 ? 60 : 115,
                   color: black,
                 ),
                 Container(
@@ -68,7 +68,7 @@ class HomeScreen extends StatelessWidget {
                   child: Container(
                     width: width >= 800 ? width - 60 : width - 30,
                     alignment: Alignment.centerLeft,
-                    height: width >= 800 ? 60 : 200,
+                    height: width >= 800 ? 60 : 170,
                     constraints: BoxConstraints(maxWidth: 1200),
                     padding: EdgeInsets.all(7),
                     decoration: BoxDecoration(
@@ -159,84 +159,87 @@ class HomeScreen extends StatelessWidget {
                           ),
                   ),
                 ),
-                Container(
-                  margin: EdgeInsets.only(top: width >= 800 ? 120 : 240),
-                  width: width >= 800 ? width - 60 : width,
-                  constraints: BoxConstraints(maxWidth: 1200),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    spacing: 12,
-                    children: [
-                      Padding(
-                        padding: width >= 800
-                            ? EdgeInsets.zero
-                            : EdgeInsets.symmetric(horizontal: 15),
-                        child: Text(
-                          "Most Viewed",
-                          style: TextStyle(
-                            color: black,
-                            fontSize: width >= 800 ? 32 : 22,
-                            fontWeight: FontWeight.bold,
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.only(top: width >= 800 ? 120 : 210),
+                    width: width >= 800 ? width - 60 : width,
+                    constraints: BoxConstraints(maxWidth: 1200),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      spacing: 12,
+                      children: [
+                        Padding(
+                          padding: width >= 800
+                              ? EdgeInsets.zero
+                              : EdgeInsets.symmetric(horizontal: 15),
+                          child: Text(
+                            "Most Viewed",
+                            style: TextStyle(
+                              color: black,
+                              fontSize: width >= 800 ? 32 : 22,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: width >= 800 ? 500 : 300,
-                        child: width >= 800
-                            ? Row(
-                                spacing: 10,
-                                children: [
-                                  Expanded(
-                                    child: Container(
-                                      color: Colors.grey.shade400,
+                        SizedBox(
+                          height: width >= 800 ? 500 : 300,
+                          child: width >= 800
+                              ? Row(
+                                  spacing: 10,
+                                  children: [
+                                    Expanded(
+                                      child: Container(
+                                        color: Colors.grey.shade400,
+                                      ),
                                     ),
-                                  ),
-                                  Expanded(
-                                    child: Column(
-                                      spacing: 10,
-                                      children: [
-                                        Expanded(
-                                          child: Container(
-                                            color: Colors.grey.shade400,
+                                    Expanded(
+                                      child: Column(
+                                        spacing: 10,
+                                        children: [
+                                          Expanded(
+                                            child: Container(
+                                              color: Colors.grey.shade400,
+                                            ),
                                           ),
-                                        ),
-                                        Expanded(
-                                          child: Row(
-                                            spacing: 10,
-                                            children: [
-                                              Expanded(
-                                                child: Container(
-                                                  color: Colors.grey.shade400,
+                                          Expanded(
+                                            child: Row(
+                                              spacing: 10,
+                                              children: [
+                                                Expanded(
+                                                  child: Container(
+                                                    color: Colors.grey.shade400,
+                                                  ),
                                                 ),
-                                              ),
-                                              Expanded(
-                                                child: Container(
-                                                  color: Colors.grey.shade400,
+                                                Expanded(
+                                                  child: Container(
+                                                    color: Colors.grey.shade400,
+                                                  ),
                                                 ),
-                                              ),
-                                            ],
+                                              ],
+                                            ),
                                           ),
-                                        ),
-                                      ],
+                                        ],
+                                      ),
                                     ),
+                                  ],
+                                )
+                              : CarouselSlider.builder(
+                                  options: CarouselOptions(
+                                    autoPlay: true,
+                                    height: double.infinity,
                                   ),
-                                ],
-                              )
-                            : CarouselSlider.builder(
-                                options: CarouselOptions(
-                                  autoPlay: true,
-                                  height: double.infinity,
+                                  itemCount: 3,
+                                  itemBuilder: (_, i, __) => Container(
+                                    height: double.infinity,
+                                    width: double.infinity,
+                                    margin:
+                                        EdgeInsets.symmetric(horizontal: 10),
+                                    color: Colors.grey.shade400,
+                                  ),
                                 ),
-                                itemCount: 3,
-                                itemBuilder: (_, i, __) => Container(
-                                  height: double.infinity,
-                                  width: double.infinity,
-                                  margin: EdgeInsets.symmetric(horizontal: 10),
-                                  color: Colors.grey.shade400,
-                                ),
-                              ),
-                      ),
-                    ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
